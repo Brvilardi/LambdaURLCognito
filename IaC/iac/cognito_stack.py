edaedae61dfb52bb1ec9258e9afafe23edf5db90
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-06-07 23:44:54 -0300

    refactor code indentation

--- IaC/iac/cognito_stack.py
+++ IaC/iac/cognito_stack.py
@@ -9,29 +9,28 @@ class CognitoStack(NestedStack):
     def __init__(self, scope: Construct) -> None:
         super().__init__(scope, "CognitoStack")
 
-        # Setup Cognito
 
         self.cognitoUserPool = cognito.UserPool(self, "UserPool",
-                                           user_pool_name="ExampleUserPool",
-                                           self_sign_up_enabled=True,
-                                           sign_in_aliases=cognito.SignInAliases(
-                                               username=True,
-                                               email=True
-                                           ),
+               user_pool_name="ExampleUserPool",
+               self_sign_up_enabled=True,
+               sign_in_aliases=cognito.SignInAliases(
+                   username=True,
+                   email=True
+               ),
 
-                                           )
+        )
 
         self.cognitoClient = self.cognitoUserPool.add_client("ExampleClient",
-                                                   auth_flows=cognito.AuthFlow(user_password=True),
-                                                   user_pool_client_name="ExampleClient"
-                                                   )
+               auth_flows=cognito.AuthFlow(user_password=True),
+               user_pool_client_name="ExampleClient"
+        )
 
         self.cognitoIdentityPool = cognito.CfnIdentityPool(self, "ExampleIdentityPool",
-                                                      allow_unauthenticated_identities=False,
-                                                      cognito_identity_providers=[
-                                                          cognito.CfnIdentityPool.CognitoIdentityProviderProperty(
-                                                              client_id=self.cognitoClient.user_pool_client_id,
-                                                              provider_name=f"cognito-idp.{self.region}.amazonaws.com/{self.cognitoUserPool.user_pool_id}"
-                                                          )
-                                                      ]
-                                                      )
+              allow_unauthenticated_identities=False,
+              cognito_identity_providers=[
+                  cognito.CfnIdentityPool.CognitoIdentityProviderProperty(
+                      client_id=self.cognitoClient.user_pool_client_id,
+                      provider_name=f"cognito-idp.{self.region}.amazonaws.com/{self.cognitoUserPool.user_pool_id}"
+                  )
+              ]
+        )
--- IaC/iac/lambda_stack.py
+++ IaC/iac/lambda_stack.py
@@ -17,25 +17,26 @@ class LambdaStack(NestedStack):
 
         # =============== SERVER =======================
         self.lambda_function_server = lambda_.Function(self, 'Lambda Function Server',
-                                                  runtime=lambda_.Runtime.PYTHON_3_9,
-                                                  code=lambda_.Code.from_asset('../lambda_functions/server'),
-                                                  handler='server.lambda_handler'
-                                                  )
+              runtime=lambda_.Runtime.PYTHON_3_9,
+              code=lambda_.Code.from_asset('../lambda_functions/server'),
+              handler='server.lambda_handler'
+              )
+
         self.lambda_function_server.add_function_url()
         # ===============================================
 
 
         # =============== SIGNUP =======================
         self.lambda_function_cognito_signup = lambda_.Function(self, 'Lambda Function Cognito Signup',
-                                                               runtime=lambda_.Runtime.PYTHON_3_9,
-                                                               code=lambda_.Code.from_asset(
-                                                                   '../lambda_functions/cognito_signup'),
-                                                               handler='cognito_signup.lambda_handler',
-                                                               environment={
-                                                                   'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
-                                                                   'CLIENT_ID': cognitoStack.cognitoClient.user_pool_client_id
-                                                               }
-                                                               )
+               runtime=lambda_.Runtime.PYTHON_3_9,
+               code=lambda_.Code.from_asset('../lambda_functions/cognito_signup'),
+               handler='cognito_signup.lambda_handler',
+               environment=
+                    {
+                       'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
+                       'CLIENT_ID': cognitoStack.cognitoClient.user_pool_client_id
+                    }
+       )
 
         self.lambda_function_cognito_signup.add_to_role_policy(
             iam.PolicyStatement(
@@ -53,31 +54,33 @@ class LambdaStack(NestedStack):
 
         # =============== LOGIN =======================
         self.lambda_function_cognito_login = lambda_.Function(self, 'Lambda Function Cognito Login',
-                                                         runtime=lambda_.Runtime.PYTHON_3_9,
-                                                         code=lambda_.Code.from_asset(
-                                                             '../lambda_functions/cognito_login'),
-                                                         handler='cognito_login.lambda_handler',
-                                                         environment={
-                                                             'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
-                                                             'CLIENT_ID': cognitoStack.cognitoClient.user_pool_client_id
-                                                         }
-                                                         )
+             runtime=lambda_.Runtime.PYTHON_3_9,
+             code=lambda_.Code.from_asset(
+                 '../lambda_functions/cognito_login'),
+             handler='cognito_login.lambda_handler',
+             environment=
+                {
+                     'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
+                     'CLIENT_ID': cognitoStack.cognitoClient.user_pool_client_id
+                }
+        )
 
         # ===============================================
 
 
         # =============== GET CREDENTIALS =======================
         self.lambda_function_cognito_get_credentials = lambda_.Function(self, 'Lambda Function Cognito Get Credentials',
-                                                                   runtime=lambda_.Runtime.PYTHON_3_9,
-                                                                   code=lambda_.Code.from_asset(
-                                                                       '../lambda_functions/cognito_get_credentials'),
-                                                                   handler='cognito_get_credentials.lambda_handler',
-                                                                   environment={
-                                                                       'IDENTITY_POOL_ID': cognitoStack.cognitoIdentityPool.ref,
-                                                                       'IDENTITY_POOL_REGION': self.region,
-                                                                       'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
-                                                                   }
-                                                                   )
+           runtime=lambda_.Runtime.PYTHON_3_9,
+           code=lambda_.Code.from_asset(
+               '../lambda_functions/cognito_get_credentials'),
+           handler='cognito_get_credentials.lambda_handler',
+           environment=
+                {
+                   'IDENTITY_POOL_ID': cognitoStack.cognitoIdentityPool.ref,
+                   'IDENTITY_POOL_REGION': self.region,
+                   'USER_POOL_ID': cognitoStack.cognitoUserPool.user_pool_id,
+                }
+       )
 
         self.lambda_function_cognito_get_credentials.add_to_role_policy(
             iam.PolicyStatement(
